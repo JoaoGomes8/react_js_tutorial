e810d42d3661676de63ee3d4e82723a8a3644bd4
JoaoGomes8 <joaogomes2609@gmail.com>
2023-01-21 19:45:32 +0000

    multiple components in one file

---
+++ react_js/15_multiple_components_same_file/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Componente1,Componente2,Componente3} from './components/Components'
+
+export default function App() {
+  return (
+    <>
+      <h3>React - Múltiplos componentes no mesmo ficheiro</h3>
+      <hr />
+      <Componente1 />
+      <Componente2 />
+      <Componente3 />
+    </>
+  )
+}
