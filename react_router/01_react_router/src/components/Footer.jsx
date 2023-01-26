0966e0471a6cbad67958a7f2d261259e78004a27
JoaoGomes8 <joaogomes2609@gmail.com>
2023-01-26 11:32:46 +0000

    React Hook - useNavigate

--- react_router/01_react_router/src/components/Footer.jsx
+++ react_router/01_react_router/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function Footer() {
     return (
         <>
-            <div className="container-fluid bg-dark text-white text-center">
+            <div className="container-fluid bg-dark text-white text-center mt-4">
                 <div className="row">
                     <div className="col text-center">
                         <p>Aplicação React com React router</p>
--- react_router/01_react_router/src/components/Home.jsx
+++ react_router/01_react_router/src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
     return (
         <section className="container">
             <div className="row justify-content-center mt-3">
                 <div className="col-8 card bg-light text-center p-3">
                     <h3>Home!</h3>
+                </div>
+                <div className="text-center mt-4">
+                    <button onClick={() =>{
+                        navigate('/services')
+                    }}>Services</button>
                 </div>
             </div>
         </section>
