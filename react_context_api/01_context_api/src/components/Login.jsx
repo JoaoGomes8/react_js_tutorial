fb94e0f0a558cb3a71f54cb1b1f258dfc0436538
JoaoGomes8 <joaogomes2609@gmail.com>
2023-01-29 11:54:59 +0000

    prop drilling

---
+++ react_context_api/01_context_api/src/components/Login.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Login({login}) {
+    return(
+        <>
+            <button onClick={() =>{
+                login('António')
+            }}>Login</button>
+        </>
+    )
+}
