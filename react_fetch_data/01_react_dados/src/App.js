60d9d9bf8da729937ec6c010b4afb1eec11f2fe8
JoaoGomes8 <joaogomes2609@gmail.com>
2023-01-31 11:15:54 +0000

    fetch data .json file

--- react_fetch_data/01_react_dados/src/App.js
+++ react_fetch_data/01_react_dados/src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 
 export default function App() {
 
+  const [cliente, setCliente] = useState({
+    nome: "",
+    email: "",
+    idade: 0
+  })
+
   useEffect(() =>{
-    fetch('http://localhost:3000/dados/texto.txt')
-    .then((res) => res.text())
-    .then((texto) => console.log(texto))
+    fetch('http://localhost:3000/dados/dados.json')
+    .then((res) => res.json())
+    .then((dados_cliente) => {
+      setCliente(dados_cliente)
+    })
+
+    console.log('teste');
   },[])
 
   return (
     <>
       <h1>React Fetch Data</h1>
+      <hr />
+      <p>Nome do cliente: {cliente.nome}</p>
+      <p>Email do cliente: {cliente.email}</p>
+      <p>Idade do cliente: {cliente.idade}</p>
     </>
   )
 }
